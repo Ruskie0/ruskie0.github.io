d01170678e81d02a846ce725cecfd4b39fe69a3d
Ruskie <102234424+Ruskie0@users.noreply.github.com>
2024-02-15 12:57:11 +0100

    Final v3

--- js/script.js
+++ js/script.js
@@ -1,109 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  gsap.from(".context", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-  });
-  gsap.from(".gsapWorkCheck", {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-  });
-  gsap.from(".gsapWorkWith", {
-    opacity: 0,
-    y: 100,
-    delay: 2,
-    duration: 1.1,
-  });
-  gsap.from(".gsapWorkWithA", {
-    opacity: 0,
-    y: 100,
-    delay: 2,
-    duration: 1.4,
-  });
-  gsap.from("#aboutMeSectionR", {
-    scrollTrigger: {
-      trigger: "#aboutMeSectionR",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.7,
-  });
-  gsap.from("#aboutMeTextR", {
-    scrollTrigger: {
-      trigger: "#aboutMeTextR",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.7,
-  });
-  gsap.from("#aboutMeSectionL", {
-    scrollTrigger: {
-      trigger: "#aboutMeSectionL",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.7,
-  });
-  gsap.from("#aboutMeTextL", {
-    scrollTrigger: {
-      trigger: "#aboutMeTextL",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.7,
-  });
-  gsap.from("#gsapWork1", {
-    scrollTrigger: {
-      trigger: "#gsapWork1",
-      toggleActions: "play none none none",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.5,
-  });
-  gsap.from("#gsapWork2", {
-    scrollTrigger: {
-      trigger: "#gsapWork2",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: 75,
-    delay: 0.5,
-    duration: 0.5,
-  });
-  gsap.from("#gsapWork3", {
-    scrollTrigger: {
-      trigger: "#gsapWork3",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: -75,
-    delay: 0.5,
-    duration: 0.5,
-  });
-  gsap.from("#gsapWork4", {
-    scrollTrigger: {
-      trigger: "#gsapWork4",
-      toggleActions: "play",
-    },
-    opacity: 0,
-    x: 75,
-    delay: 0.5,
-    duration: 0.5,
-  });
-
   var splide = new Splide(".splide", {
     type: "loop",
+    height: "autoWidth",
     gap: "20px",
     perPage: 3,
     focus: 0,
@@ -119,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   var splide2 = new Splide("#splide2", {
     type: "loop",
-    height: "autoHeight",
+    height: "autoWidth",
     gap: "20px",
     perPage: 3,
     focus: 0,
@@ -135,10 +33,231 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide2.mount();
   splide.mount();
-});
 
-gsap.from(".gsapTest", {
-  opacity: 0,
-  x: 100,
-  duration: 1,
+  function myFunction(watchRes) {
+    if (watchRes.matches) {
+      // If media query matches
+      gsap.from(".context", {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+      });
+      gsap.from(".gsapWorkCheck", {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+      });
+      gsap.from(".gsapWorkWith", {
+        opacity: 0,
+        y: 100,
+        delay: 2,
+        duration: 1.1,
+      });
+      gsap.from(".gsapWorkWithA", {
+        opacity: 0,
+        y: 100,
+        delay: 2,
+        duration: 1.4,
+      });
+      gsap.from("#aboutMeSectionR", {
+        scrollTrigger: {
+          trigger: "#aboutMeSectionR",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeTextR", {
+        scrollTrigger: {
+          trigger: "#aboutMeTextR",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeSectionL", {
+        scrollTrigger: {
+          trigger: "#aboutMeSectionL",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeTextL", {
+        scrollTrigger: {
+          trigger: "#aboutMeTextL",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#gsapWork1", {
+        scrollTrigger: {
+          trigger: "#gsapWork1",
+          toggleActions: "play none none none",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork2", {
+        scrollTrigger: {
+          trigger: "#gsapWork2",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: 75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork3", {
+        scrollTrigger: {
+          trigger: "#gsapWork3",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork4", {
+        scrollTrigger: {
+          trigger: "#gsapWork4",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: 75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+    } else {
+      gsap.from(".context", {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+      });
+      gsap.from(".gsapWorkCheck", {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+      });
+      gsap.from(".gsapWorkWith", {
+        opacity: 0,
+        y: 100,
+        delay: 2,
+        duration: 1.1,
+      });
+      gsap.from(".gsapWorkWithA", {
+        opacity: 0,
+        y: 100,
+        delay: 2,
+        duration: 1.4,
+      });
+      gsap.from("#aboutMeSectionR", {
+        scrollTrigger: {
+          trigger: "#aboutMeSectionR",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeTextR", {
+        scrollTrigger: {
+          trigger: "#aboutMeTextR",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeSectionL", {
+        scrollTrigger: {
+          trigger: "#aboutMeSectionL",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#aboutMeTextL", {
+        scrollTrigger: {
+          trigger: "#aboutMeTextL",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.7,
+      });
+      gsap.from("#gsapWork1", {
+        scrollTrigger: {
+          trigger: "#gsapWork1",
+          toggleActions: "play none none none",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork2", {
+        scrollTrigger: {
+          trigger: "#gsapWork2",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork3", {
+        scrollTrigger: {
+          trigger: "#gsapWork3",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+      gsap.from("#gsapWork4", {
+        scrollTrigger: {
+          trigger: "#gsapWork4",
+          toggleActions: "play",
+        },
+        opacity: 0,
+        x: -75,
+        delay: 0.5,
+        duration: 0.5,
+      });
+    }
+  }
+
+  var watchRes = window.matchMedia("(min-width: 1320px)");
+
+  // Call listener function at run time
+  myFunction(watchRes);
+
+  // Attach listener function on state changes
+  watchRes.addEventListener("change", function () {
+    myFunction(watchRes);
+  });
+
+  gsap.from(".gsapTest", {
+    opacity: 0,
+    x: 100,
+    duration: 1,
+  });
 });
